f1c6f560e6a2ed6edc10b5daf8ab05d7e7514c00
Ing. Tomáš Lašo <laso@winkros.local>
2020-05-13 09:23:12 +0200

    Pridanie komentu.

--- viewer/tilingrenderlayer.js
+++ viewer/tilingrenderlayer.js
@@ -182,6 +182,7 @@ export class TilingRenderLayer extends RenderLayer {
 		}
 	}
 	
+	// Changed: oprava chyby - do visibleElements prisla hodnota false
 	renderBuffers(transparency, reuse, lines, visibleElements) {
 		// TODO when navigation is active (rotating, panning etc...), this would be the place to decide to for example not-render anything in this layer, or maybe apply more aggressive culling
 		// if (this.viewer.navigationActive) {
